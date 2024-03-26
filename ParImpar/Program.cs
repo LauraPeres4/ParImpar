eab8c9ea576d88cf15efd9f7c2c31fb9590eb9ad
Usuario <Usuario@CPD02-M16>
2024-03-26 11:00:31 -0300

    Adicionar arquivos de projeto.

---
+++ ParImpar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 0 para pares e 1 para impares: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int cont = num;
+
+            while (cont <= 20)
+            {
+                Console.WriteLine(cont);
+                cont += 2;
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
